b8f1a7e2e7a9698645bab99794627e7bee453aee
Andy Weir <OceanInternet@users.noreply.github.com>
2017-08-15 10:44:40 +0100

    add extract user agent

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ module.exports.handleSuccess     = handleSuccess;
 module.exports.handleError       = handleError;
 module.exports.extractNew        = extractNew;
 module.exports.extractData       = extractData;
+module.exports.extractUserAgent  = extractUserAgent;
 module.exports.getPathParam      = getPathParam;
 
 
@@ -81,4 +82,8 @@ function extractData(event) {
 function getPathParam(param, event) {
 
     return event.pathParameters[param];
+}
+
+function extractUserAgent(event) {
+    return event.headers['User-Agent'];
 }
